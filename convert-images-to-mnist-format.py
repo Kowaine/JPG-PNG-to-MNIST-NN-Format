88d8c59e47354fd8192686e00800febfde9afd6b
Kowaine <31977607+Kowaine@users.noreply.github.com>
2019-12-18 15:29:09 +0800

    针对自己的使用需求修改了部分代码

--- convert-images-to-mnist-format.py
+++ convert-images-to-mnist-format.py
@@ -2,72 +2,77 @@ import os
 from PIL import Image
 from array import *
 from random import shuffle
+import numpy as np
 
 # Load from and save to
-Names = [['./training-images','train'], ['./test-images','test']]
+Names = [['training-images','train'], ['t10k-images','t10k']]
 
 for name in Names:
-	
-	data_image = array('B')
-	data_label = array('B')
+    
+    data_image = array('B')
+    #data_image = []
+    data_label = array('B')
 
-	FileList = []
-	for dirname in os.listdir(name[0])[1:]: # [1:] Excludes .DS_Store from Mac OS
-		path = os.path.join(name[0],dirname)
-		for filename in os.listdir(path):
-			if filename.endswith(".png"):
-				FileList.append(os.path.join(name[0],dirname,filename))
+    FileList = []
+    for dirname in os.listdir(name[0]): # [1:] Excludes .DS_Store from Mac OS
+        path = os.path.join(name[0],dirname)
+        for filename in os.listdir(path):
+            if filename.endswith(".png"):
+                FileList.append(os.path.join(name[0],dirname,filename))
 
-	shuffle(FileList) # Usefull for further segmenting the validation set
+    shuffle(FileList) # Usefull for further segmenting the validation set
 
-	for filename in FileList:
+    if len(FileList) == 0:
+        continue
 
-		label = int(filename.split('/')[2])
+    for filename in FileList:
 
-		Im = Image.open(filename)
+        label = int(filename.split('\\')[1])
 
-		pixel = Im.load()
+        Im = Image.open(filename).convert("L")
 
-		width, height = Im.size
+        pixel = Im.load()
 
-		for x in range(0,width):
-			for y in range(0,height):
-				data_image.append(pixel[y,x])
+        width, height = Im.size
 
-		data_label.append(label) # labels start (one unsigned byte each)
+        for x in range(0,width):
+            for y in range(0,height):
+                data_image.append(pixel[y, x])
 
-	hexval = "{0:#0{1}x}".format(len(FileList),6) # number of files in HEX
+        data_label.append(label) # labels start (one unsigned byte each)
 
-	# header for label array
+    hexval = "{0:#0{1}x}".format(len(FileList),6) # number of files in HEX
 
-	header = array('B')
-	header.extend([0,0,8,1,0,0])
-	header.append(int('0x'+hexval[2:][:2],16))
-	header.append(int('0x'+hexval[2:][2:],16))
-	
-	data_label = header + data_label
+    # header for label array
 
-	# additional header for images array
-	
-	if max([width,height]) <= 256:
-		header.extend([0,0,0,width,0,0,0,height])
-	else:
-		raise ValueError('Image exceeds maximum size: 256x256 pixels');
+    header = array('B')
+    header.extend([0,0,8,1,0,0])
+    header.append(int('0x'+hexval[2:][:2],16))
+    header.append(int('0x'+hexval[2:][2:],16))
+    
+    data_label = header + data_label
 
-	header[3] = 3 # Changing MSB for image data (0x00000803)
-	
-	data_image = header + data_image
+    # additional header for images array
+    
+    if max([width,height]) <= 256:
+        header.extend([0,0,0,width,0,0,0,height])
+    else:
+        raise ValueError('Image exceeds maximum size: 256x256 pixels');
 
-	output_file = open(name[1]+'-images-idx3-ubyte', 'wb')
-	data_image.tofile(output_file)
-	output_file.close()
+    header[3] = 3 # Changing MSB for image data (0x00000803)
+    
+    data_image = header + data_image
 
-	output_file = open(name[1]+'-labels-idx1-ubyte', 'wb')
-	data_label.tofile(output_file)
-	output_file.close()
+    output_file = open(name[1]+'-images-idx3-ubyte', 'wb')
+    data_image.tofile(output_file)
+    output_file.close()
+
+    output_file = open(name[1]+'-labels-idx1-ubyte', 'wb')
+    data_label.tofile(output_file)
+    output_file.close()
 
 # gzip resulting files
 
 for name in Names:
-	os.system('gzip '+name[1]+'-images-idx3-ubyte')
-	os.system('gzip '+name[1]+'-labels-idx1-ubyte')
+    os.system('7z a -tgzip '+name[1]+'-images-idx3-ubyte.gz '+name[1]+'-images-idx3-ubyte')
+    os.system('7z a -tgzip '+name[1]+'-labels-idx1-ubyte.gz '+name[1]+'-labels-idx1-ubyte')
